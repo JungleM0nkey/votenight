451cef18f8368e36f39f29539e62c2cc63c3bf91
Ilya Shevchenko <ilya.93@hotmail.com>
2020-12-03 15:46:02 -0500

    Temporary removing email pw reset

--- app/routes.py
+++ app/routes.py
@@ -560,17 +560,17 @@ def applymoviechanges():
 #        return redirect(url_for('login'))
 #    return render_template('pwreset.html', form=form)
 
-@app.route('/reset_password/<token>', methods=['GET', 'POST'])
-def reset_password(token):
-    if current_user.is_authenticated:
-        return redirect(url_for('index'))
-    user = User.verify_reset_password_token(token)
-    if not user:
-        return redirect(url_for('index'))
-    form = ResetPasswordForm()
-    if form.validate_on_submit():
-        user.set_password(form.password.data)
-        db.session.commit()
-        flash('Your password has been reset.')
-        return redirect(url_for('login'))
-    return render_template('changepw.html', form=form)
+#@app.route('/reset_password/<token>', methods=['GET', 'POST'])
+#def reset_password(token):
+#    if current_user.is_authenticated:
+#        return redirect(url_for('index'))
+#    user = User.verify_reset_password_token(token)
+#    if not user:
+#        return redirect(url_for('index'))
+#    form = ResetPasswordForm()
+#    if form.validate_on_submit():
+#        user.set_password(form.password.data)
+#        db.session.commit()
+#        flash('Your password has been reset.')
+#        return redirect(url_for('login'))
+#    return render_template('changepw.html', form=form)
