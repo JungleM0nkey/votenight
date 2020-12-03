41937df6457d03836b4d4e6f0d0ecc904aaae290
Ilya Shevchenko <ilya.93@hotmail.com>
2020-12-03 15:21:08 -0500

    Added azure shim file

--- config.py
+++ config.py
@@ -18,3 +18,4 @@ class Config(object):
     MAIL_USERNAME = os.environ.get('MAIL_USERNAME')
     MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD')
     ADMINS = ['votenight.pw@gmail.com']
+    FLASK_RUN_PORT=8000
---
+++ startup.py
@@ -0,0 +1,3 @@
+# startup.py shim
+
+from app import app
