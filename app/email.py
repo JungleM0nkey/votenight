d507c6e0bf87d100d7589135f4b656527d690d69
Ilya Shevchenko <ilya.93@hotmail.com>
2020-12-03 16:37:04 -0500

    Fixed js and email files missing

---
+++ app/email.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python3 
+
+
+from flask_mail import Message
+from app import app
+from app import mail
+from flask import render_template
+
+def send_email(subject, sender, recipients, text_body, html_body):
+    msg = Message(subject, sender=sender, recipients=recipients)
+    msg.body = text_body
+    msg.html = html_body
+    mail.send(msg)
+
+
+def send_password_reset_email(user):
+    token = user.get_reset_password_token()
+    send_email('Votenight Reset Your Password',
+               sender=app.config['ADMINS'][0],
+               recipients=[user.email],
+               text_body=render_template('email/reset_password.txt',
+                                         user=user, token=token),
+               html_body=render_template('email/reset_password.html',
+                                         user=user, token=token))
+
+def send_email(subject, sender, recipients, text_body, html_body):
+    msg = Message(subject, sender=sender, recipients=recipients)
+    msg.body = text_body
+    msg.html = html_body
+    with app.app_context(): 
+        mail.send(msg) 
